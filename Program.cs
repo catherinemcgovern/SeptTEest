a2c012d8a12ffdc6931f1b9464d08399ba16d641
cmcgovern4ever <cmcgovern@wtamu.edu>
2018-09-22 08:17:11 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjLyndASP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
